cd888fb156d6cae16b71069d00baa498b5f98c04
svvimming <benmunozj@gmail.com>
2023-12-08 18:56:00 -0500

    feat: animated card and numbers

--- packages/site/components/section-infographic.vue
+++ packages/site/components/section-infographic.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="infographic">
-
+  <section class="infographic-container">
+    
     <div class="section-heading">
       <div class="grid-noGutter-noBottom">
         <div class="col-6">
@@ -11,102 +11,143 @@
       </div>
     </div>
 
-    <div class="section-info">
+    <div class="infographic">
 
-    </div>
+      <div class="section-info-card">
+        <div class="card-track">
+          <div
+            v-if="card"
+            ref="cardRef"
+            class="info-card"
+            :style="{ transform: `translate(${cardX}px, -21px)`, height: cardHeight ? `${cardHeight + 40}px` : 'unset' }">
+            <div ref="cardContent" class="card-content">
 
-    <div class="section-graphics">
+              <div class="card-number">
+                <div class="number highlight-solid">
+                  <div class="number-text">
+                    {{ activeIndex }}
+                  </div>
+                </div>
+              </div>
 
-      <div
-        v-for="(stage, n) in stages"
-        :key="stage.name"
-        :class="['stage', `stage-${n + 1}`, { active: activeIndex >= n + 1 }]"
-        :style="{ 'flex-basis': `${stage.width}%`, 'max-width': `${stage.width}%` }">
+              <div class="heading">
+                {{ card.heading }}
+              </div>
 
-        <div class="number-wrapper">
-          <div class="number">
-            {{ n + 1 }}
-          </div>
-        </div>
+              <div class="description">
+                {{ card.description }}
+              </div>
 
-        <div class="name">
-          <div class="text blue-border border-md">
-            {{ stage.name }}
-          </div>
-        </div>
+              <div class="nav-button">
 
-        <div
-          v-if="n + 1 === 1"
-          class="tenant blue-border border-lg">
-          <div class="tenants">
-            <div v-for="i in 8" :key="`folder-${i}`" class="folder">
-              <IconFolder class="icon" />
+              </div>
+
             </div>
           </div>
-          <div class="collections">
-            Collections
-          </div>
         </div>
+      </div>
+
+      <div class="section-graphics">
+
+        <div class="numbers-track" :style="{ '--track-highlight-width': `${cardX}px` }"></div>
 
         <div
-          v-if="n + 1 === 2"
-          class="policy">
-          <svg
-            class="dashed-line"
-            xmlns="http://www.w3.org/2000/svg"
-            width="11"
-            height="381"
-            viewBox="0 0 11 381"
-            fill="none">
-            <path
-              d="M5.91406 0.109375L5.91408 380.828"
-              stroke="#E8FF5A"
-              stroke-width="10"
-              stroke-dasharray="4 7" />
-          </svg>
-          <div class="arrow-wrapper">
-            <InfographicArrow class="arrow" />
-          </div>
-        </div>
+          v-for="(stage, n) in stages"
+          ref="stagesRef"
+          :key="stage.name"
+          :class="['stage', `stage-${n + 1}`, { active: activeIndex >= n + 1 }]"
+          :style="{ 'flex-basis': `${stage.width}%`, 'max-width': `${stage.width}%` }">
 
-        <div
-          v-if="n + 1 === 3"
-          class="broker-service">
-          <div class="logo blue-border border-lg">
-            <SiteLogo />
+          <div class="number-wrapper" :style="{ height: cardHeight ? `${cardHeight + 40}px` : 'unset' }">
+            <button
+              :class="['number', { hidden: activeIndex === n + 1 }]"
+              @click="activeIndex = n + 1"
+              :style="{ transform: `translateX(${numberOffsets[n]}px)` }">
+              <div class="number-text">
+                {{ n + 1 }}
+              </div>
+            </button>
           </div>
-          <div class="arrow-wrapper">
-            <InfographicArrow class="arrow" />
-          </div>
-        </div>
 
-        <div
-          v-if="n + 1 === 4"
-          class="storage-provider">
-          <div class="sps">
-            <div
-              v-for="i in 7"
-              :key="`sp-col-${i}`"
-              :class="['sp-col', { offset: i % 2 === 0 }]">
+          <div class="name">
+            <div class="text blue-border border-md">
+              {{ stage.name }}
+            </div>
+          </div>
+
+          <div
+            v-if="n + 1 === 1"
+            class="tenant blue-border border-lg">
+            <div class="tenants">
+              <div v-for="i in 8" :key="`folder-${i}`" class="folder">
+                <IconFolder class="icon" />
+              </div>
+            </div>
+            <div class="collections">
+              Collections
+            </div>
+          </div>
+
+          <div
+            v-if="n + 1 === 2"
+            class="policy">
+            <svg
+              class="dashed-line"
+              xmlns="http://www.w3.org/2000/svg"
+              width="11"
+              height="381"
+              viewBox="0 0 11 381"
+              fill="none">
+              <path
+                d="M5.91406 0.109375L5.91408 380.828"
+                stroke="#E8FF5A"
+                stroke-width="10"
+                stroke-dasharray="4 7" />
+            </svg>
+            <div class="arrow-wrapper">
+              <InfographicArrow class="arrow" />
+            </div>
+          </div>
+
+          <div
+            v-if="n + 1 === 3"
+            class="broker-service">
+            <div class="logo blue-border border-lg">
+              <SiteLogo />
+            </div>
+            <div class="arrow-wrapper">
+              <InfographicArrow class="arrow" />
+            </div>
+          </div>
+
+          <div
+            v-if="n + 1 === 4"
+            class="storage-provider">
+            <div class="sps">
               <div
-                v-for="j in 3"
-                :key="`sp-${i}-${j}`"
-                class="sp blue-border border-sm">
-                <IconServer />
-                <span>
-                  SP
-                </span>
+                v-for="i in 7"
+                :key="`sp-col-${i}`"
+                :class="['sp-col', { offset: i % 2 === 0 }]">
+                <div
+                  v-for="j in 3"
+                  :key="`sp-${i}-${j}`"
+                  class="sp blue-border border-sm">
+                  <IconServer />
+                  <span>
+                    SP
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
+        </div>
 
       </div>
 
     </div>
 
-  </div>
+  </section>
 </template>
 
 <script setup>
@@ -119,9 +160,56 @@ const props = defineProps({
   }
 })
 
+const cardRef = ref(null)
+const cardContent = ref(null)
+const stagesRef = ref(null)
 const activeIndex = ref(1)
+const cardX = ref(0)
+const cardHeight = ref(0)
+const numberOffsets = ref([0, 0, 0, 0])
 
+// ==================================================================== Computed
 const stages = computed(() => props.block.stages)
+const card = computed(() => stages.value[activeIndex.value - 1]?.card)
+
+// ==================================================================== Watchers
+watch(activeIndex, (val) => {
+  moveCardTo(val)
+})
+
+watch(card, () => {
+  nextTick(() => {
+    cardHeight.value = cardContent.value.getBoundingClientRect().height
+  })
+}, { deep: true })
+
+// ======================================================================= Hooks
+// onMounted(() => {
+//   setTimeout(() => {
+//     nextTick(() => {
+//       moveCardTo(1)
+//     })
+//   }, 1000)
+// })
+
+// ===================================================================== Methods
+const moveCardTo = (index) => {
+  if (stagesRef.value && cardRef.value && cardContent.value) {
+    const stageWidths = stagesRef.value.map(stg => stg.getBoundingClientRect().width)
+    const currentStage = stagesRef.value[index - 1]
+    const width = currentStage.getBoundingClientRect().width
+    const cardWidth = cardRef.value.getBoundingClientRect().width
+    cardX.value = currentStage.offsetLeft + (width - cardWidth) * 0.5
+    const arr = numberOffsets.value.map((_, i) => {
+      const current = index - 1
+      if (i === current) { return 0 }
+      const dif = -1 * (current - i)
+      return (2 / dif) * (100 / stageWidths[i]) * 100
+    })
+    numberOffsets.value = arr
+    console.log(arr)
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -150,8 +238,28 @@ const stages = computed(() => props.block.stages)
 }
 
 // //////////////////////////////////////////////////////////////////// Controls
+.section-info-card {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 0;
+  // transform: translateY(100%);
+  z-index: 10;
+}
+
+.card-track {
+  height: 0;
+}
+
 .number-wrapper {
   position: relative;
+  display: flex;
+  justify-content: center;
+  transition: height 300ms ease;
+}
+
+.number {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -160,9 +268,21 @@ const stages = computed(() => props.block.stages)
   width: toRem(30);
   height: toRem(30);
   background-color: $blackPearl;
+  transition: 300ms ease;
+  opacity: 1;
+  &.hidden {
+    opacity: 0;
+  }
+  &.highlight-solid {
+    background-color: $canary;
+    border-color: $canary;
+    .number-text {
+      color: $woodsmoke;
+    }
+  }
 }
 
-.number {
+.number-text {
   text-align: center;
   font-family: $fontSuisseIntl;
   font-size: toRem(18);
@@ -171,24 +291,78 @@ const stages = computed(() => props.block.stages)
   letter-spacing: 0.36px;
   color: $cornflowerBlue;
 }
+
+.info-card {
+  padding: toRem(20) toRem(23) toRem(20) toRem(75);
+  width: 37%;
+  border: solid 1px $cornflowerBlue;
+  border-radius: toRem(10);
+  background-color: $blackPearl;
+  transition: 300ms ease;
+  overflow: hidden;
+  .heading,
+  .description {
+    color: $athensGray;
+    font-family: $fontSuisseIntl;
+    font-size: toRem(16);
+    line-height: leading(22, 16);
+  }
+  .heading {
+    font-weight: 500;
+  }
+}
+
+.card-content {
+  position: relative;
+}
+
+.card-number {
+  position: absolute;
+  top: 0;
+  left: toRem(-55);
+}
+
 // //////////////////////////////////////////////////////////////////// Graphics
 .section-graphics {
   position: relative;
   display: flex;
   justify-content: space-around;
-  &:before {
+}
+
+.numbers-track {
+  --track-highlight-width: 0;
+  position: absolute;
+  left: 0;
+  top: toRem(14);
+  width: 100%;
+  height: 2px;
+  &:before,
+  &:after {
     content: '';
     position: absolute;
+    top: 0;
     left: 0;
-    top: toRem(14);
     width: 100%;
-    height: 2px;
+    height: 100%;
+  }
+  &:before {
     background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='969' height='3' viewBox='0 0 969 3' fill='none'%3e%3cpath d='M968.594 1.51562H0.875' stroke='%23648BF5' stroke-width='2' stroke-dasharray='4 7'/%3e%3c/svg%3e");
+  }
+  &:after {
+    width: var(--track-highlight-width);
+    transition: width 300ms ease;
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='969' height='3' viewBox='0 0 969 3' fill='none'%3e%3cpath d='M968.594 1.51562H0.875' stroke='%23E8FF5A' stroke-width='2' stroke-dasharray='4 7'/%3e%3c/svg%3e");
   }
 }
 
 .stage {
   &.active {
+    .number {
+      border-color: $canary;
+      .number-text {
+        color: $canary;
+      }
+    }
     .blue-border {
       &:before {
         opacity: 1;
@@ -205,15 +379,10 @@ const stages = computed(() => props.block.stages)
   }
 }
 
-// .stage-2 {
-//   .text {
-//     background-color: $blackPearl;
-//   }
-// }
-
 .name {
   display: flex;
   justify-content: center;
+  margin-top: toRem(52);
   margin-bottom: toRem(22);
   .text {
     text-align: center;
