80222a793424edd99376b3e06172a69df376caa6
Nauras Jabari <nauras@agencyundone.com>
2023-12-13 17:25:54 -0500

    chore: test deploy

--- packages/zero-docs/nuxt.config.js
+++ packages/zero-docs/nuxt.config.js
@@ -55,6 +55,9 @@ export default defineNuxtConfig({
   },
   // ========================================================== [Module] Content
   content: {
+    experimental: {
+      clientDb: true
+    },
     sources: {
       targetDocs: {
         driver: 'fs',
