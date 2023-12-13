4ebc6559053b8cfe671698a71961998b347fe9c6
tikagan <30419893+tikagan@users.noreply.github.com>
2023-12-13 15:44:44 -0500

    feat: error page

--- packages/zero-docs/layouts/docs.vue
+++ packages/zero-docs/layouts/docs.vue
@@ -10,7 +10,11 @@
 
     <slot />
 
-    <SiteFooter />
+    <div class="grid">
+      <div class="col-10" data-push-left="off-2">
+        <SiteFooter />
+      </div>
+    </div>
 
   </div>
 </template>
