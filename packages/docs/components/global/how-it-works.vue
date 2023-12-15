0f85c55e5b004a6b1b743eb0b9890d07c1923f91
tikagan <30419893+tikagan@users.noreply.github.com>
2023-12-15 14:44:43 -0500

    feat: how it works miro embed component

---
+++ packages/docs/components/global/how-it-works.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="miro-embed">
+    <iframe
+      class="responsive-iframe"
+      src="https://miro.com/app/live-embed/uXjVMdqgtPs=/?moveToViewport=-1758,-1612,1544,919"
+      frameBorder="0"
+      scrolling="no"
+      allowFullScreen>
+
+    </iframe>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+// ///////////////////////////////////////////////////////////////////// General
+.miro-embed {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  padding-top: 56.25%; /* 16:9 Aspect Ratio */
+}
+
+.responsive-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
