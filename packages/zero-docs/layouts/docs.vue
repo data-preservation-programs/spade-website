f40ee7ec8323798019b1721afad3b35d5032fcaa
Nauras Jabari <nauras@agencyundone.com>
2023-12-18 14:09:41 -0500

    feat: added nuxt loading indicator for URL changes

--- packages/zero-docs/layouts/docs.vue
+++ packages/zero-docs/layouts/docs.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="layout default">
 
+    <NuxtLoadingIndicator />
+
     <!-- must be placed behind a v-if="algoliaEnabled" -->
     <AlgoliaModal v-if="algoliaEnabled" />
 
